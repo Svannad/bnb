8444ac89df52e9bedbf9c8ad33f2f23e0e4757af
Svannad <svannadam@gmail.com>
2025-06-05 18:46:12 +0200

    við tí fyrsta commit app set up log in sign up set up done

--- app/routes/profile.tsx
+++ app/routes/profile.tsx
@@ -1,21 +1,26 @@
-import { Form, redirect, type LoaderFunctionArgs } from "react-router";
+import { Form, Link, redirect, useLoaderData } from "react-router";
 import type { Route } from "./+types/home";
-import { sessionStorage } from "~/services/session.server"; // adjust path to your file
+import {
+  getSession,
+  sessionStorage,
+} from "~/services/session.server"; // adjust path to your file
+import User from "~/models/User";
+import type { UserType } from "~/models/User"; // or wherever it's defined
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "BNB" }];
 }
 
-export async function loader({ request }: LoaderFunctionArgs) {
-  const session = await sessionStorage.getSession(request.headers.get("Cookie"));
+export async function loader({ request }: Route.LoaderArgs) {
+  const session = await getSession(request.headers.get("cookie"));
   const authUserId = session.get("authUserId");
 
   if (!authUserId) {
     throw redirect("/signin");
   }
 
-  // If user is authenticated, return some data or just `null`
-  return null;
+  const user = await User.findById(authUserId).lean();
+  return { user };
 }
 
 export async function action({ request }) {
@@ -30,11 +35,27 @@ export async function action({ request }) {
 }
 
 export default function Profile() {
+  const { user } = useLoaderData() as { user: UserType };
+
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-500">Profil</h1>
       <p>Brúkara Info</p>
       <p>Bookings</p>
+      <section className="flex flex-col items-center text-center w-full">
+        <div className="flex flex-row justify-between items-center w-full mb-12">
+          <span className="w-[25px]"></span>
+          <h1 className="font-serif text-2xl font-medium text-left text-[#48302D]">
+            My Profile
+          </h1>
+          <Link to="/profile/update"></Link>
+        </div>
+
+        <div>
+          <h1 className="text-2xl font-medium text-[#48302D]">{user.name}</h1>
+          <p className="text-[#48302D]">{user.mail}</p>
+        </div>
+      </section>
       <Form method="post">
         <button>Logout</button>
       </Form>
